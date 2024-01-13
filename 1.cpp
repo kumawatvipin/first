7a7550a3ea67c82ecbb30762c1d65a3a1e37a04a
AVVKavvk <kumawatvipin066@gmail.com>
2024-01-13 19:37:36 +0530

    some

---
+++ 1.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+#define pi 3.14
+#define fli(a,b) for(int i=a;i<b;i++)
+#define flj(a,b) for(int j=a;j<b;j++)
+#define flk(a,b) for(int k=a;k<b;k++)
+#define ma(a,b)      ( (a) > (b) ? (a) : (b)); 
+#define mi(a,b)      ( (a) < (b) ? (a) : (b)) ;
+#define pb push_back 
+#define pob pop_back 
+#define sz size() 
+#define p2s(a,b) std::cout <<a<<' ' << b << std::endl ;
+#define pn(a) std::cout <<a << std::endl ;
+#define nl std::cout << std::endl ;
+#define p(a) std::cout <<a ;
+#define ps(a) std::cout <<a<< ' ' ;
+#define cig(arr,n)   cin.getline(arr,n) 
+#define sza(a) sizeof(a)/sizeof(int)
+#define vec vector<char> 
+#define v2e vector <vector<int> > 
+#define v2ec vector <vector<char> >
+#define vei vector<int> 
+#define pu(n) push_back( n); 
+ 
+
+int main() {
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout<<"vipin";
+return 0 ;
+}
